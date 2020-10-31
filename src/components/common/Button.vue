9f15dc895fbd19f669b0b0b2737ef3f0b8f80e7b
baiyunsong <baiyunsong@kingsoft.com>
2020-10-31 12:38:06 +0800

    [update: feat] 组件开发

---
+++ src/components/common/Button.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="__inb __cursor btn-container" :style="{ ...sty }" @click="$emit('click')">
+    <span class="btn-text">{{text}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'btn-com',
+
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    sty: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.btn-container {
+  border-radius: 4px;
+  overflow: hidden;
+  color: var(--btn-text-color);
+}
+</style>
---
+++ src/components/common/Dashedline.vue
@@ -0,0 +1,27 @@
+<template>
+  <p class="split-line" :style="{ backgroundSize: `${size * 2 }px 1px`}"></p>
+</template>
+
+<script>
+export default {
+  name: 'dashed-line-com',
+
+  props: {
+    // 虚线大小
+    size: {
+      type: Number,
+      default: 9,
+    }
+  }
+
+}
+</script>
+
+<style scoped lang="scss">
+  .split-line {
+    width: 100%;
+    height: 1px;
+    background-image: linear-gradient(to right, var(--split-line-color) 0%, var(--split-line-color), 50%, transparent 50%);
+    background-repeat: repeat-x;
+  }
+</style>
