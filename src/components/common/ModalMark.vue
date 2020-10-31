ec0f8b81979f0afb535678f79c7da7bdea37e78e
baiyunsong <baiyunsong@kingsoft.com>
2020-10-31 15:52:00 +0800

    [update:feat] 添加自定义文件命名

---
+++ src/components/common/ModalMark.vue
@@ -0,0 +1,152 @@
+<template>
+  <transition name="modalmark">
+    <div class="modal-mark-container" v-show="isShowModalMark">
+      <!-- 文件重命名 -->
+      <transition name="rename">
+        <div class="__abs rename-box" v-show="renameModalShow">
+          <p class="resume-title">{{'当前文件已存在,将根据您的输入重新创建'}}</p>
+          <div class="input-box">
+            <input ref="inputRename" type="text" placeholder="请输入文件名" v-model="rename" />
+          </div>
+          <div class="__flex __rsbc button-box">
+            <Btn class="__cursor cancel" @click="cancel()" text="取消"></Btn>
+            <Btn class="__cursor confirm" @click="confirm()" text="确认"></Btn>
+          </div>
+        </div>
+      </transition>
+    </div>
+  </transition>
+</template>
+
+<script>
+import Btn from './Button.vue';
+
+export default {
+  name: 'modal-mark-com',
+
+  components: { Btn },
+
+  data() {
+    return {
+      isShowModalMark: false,
+      renameModalShow: false,
+      rename: ''
+    }
+  },
+
+  methods: {
+    handleModalMark(bool = true) {
+      this.isShowModalMark = bool;
+      this.handleRenameShowStatus(bool);
+    },
+
+    handleRenameShowStatus(bool = true) {
+      this.renameModalShow = bool;
+    },
+
+    cancel() {
+      this.rename = '';
+      this.handleModalMark(false);
+    },
+
+    confirm() {
+      if (!this.rename.trim()) {
+        this.$toast.show({ msg: '请输入文件名' })
+        return;
+      }
+      this.$emit('click', this.rename.trim());
+      this.rename = '';
+    }
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.modal-mark-container {
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  z-index: 99;
+  background: var(--modal-mark-container-bg-color);
+
+  .rename-box {
+    background: var(--app-bg-color);
+    width: 400px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 10px 20px;
+    border-radius: 5px;
+    overflow: hidden;
+    .resume-title {
+      margin-bottom: 10px;
+    }
+
+    .input-box {
+      width: 100%;
+      height: 20px;
+      border-bottom: 1px solid var(--modal-mark-input-berder-bottom);
+      input {
+        width: 100%;
+        height: 100%;
+        border: none;
+        background: transparent;
+      }
+    }
+
+    .button-box {
+      margin-top: 15px;
+      .cancel, .confirm {
+        font-size: 16px;
+        font-weight: 400;
+        line-height: 19px;
+        height: 19px;
+      }
+      .cancel {
+        color: var(--modal-mark-rename-btn-cancel);
+      }
+      .confirm {
+        color: var(--modal-mark-rename-btn-confirm);
+      }
+    }
+  }
+
+}
+
+// 黑色遮罩动画
+.modalmark-enter-active {
+  animation: modalmark-in 0s linear;
+}
+.modalmark-leave-active {
+  animation: modalmark-out 0s linear 0.2s;
+}
+
+@keyframes modalmark-in {
+  0% { opacity: 0.6; }
+  100% { opacity: 1; }
+}
+@keyframes modalmark-out {
+  0% { opacity: 1; }
+  100% { opacity: 0; }
+}
+
+.rename-enter-active {
+  animation: rename-in 0.2s linear;
+}
+
+.rename-leave-active {
+  animation: rename-out 0.2s linear;
+}
+
+@keyframes rename-in {
+  0% { top: 55%; }
+  100% { top: 50%; }
+}
+@keyframes rename-out {
+  0% { opacity: 1; }
+  100% { opacity: 0; }
+}
+</style>
